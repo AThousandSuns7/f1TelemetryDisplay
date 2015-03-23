b23e6c7a7c13de42611388feb92b4400e286cb2f
chris <chris@webarts.ca>
2015-03-22 20:17:35 -0700

    Revs prints

--- getData.py
+++ getData.py
@@ -1,1 +1,21 @@
-test
+import time
+from lxml import html
+import requests
+import json
+
+url = "http://localhost:8002/JSON/telemetrypacket"
+page = requests.get(url)
+print (page.text)
+
+while True:
+	page = requests.get(url)
+	decoded = json.loads(page.text)
+	revs= decoded['EngineRevs']
+	print (revs)
+	time.sleep(.1)
+	
+	
+	1824.10
+	1808.63
+	1838.38
+	
