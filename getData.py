eb3ec20c1093ed8b8f54b4e7a5236e59dc6a4a69
AThousandSuns7 <chris@webarts.ca>
2015-03-24 12:35:59 -0700

    LED lighting fixes
    Cleaned up syntax and led gpio control. Actually works, will work on removing page scraping and move to a receiving packets via UDP.

--- getData.py
+++ getData.py
@@ -9,32 +9,32 @@ url = "http://amgwo5:8002/JSON/telemetrypacket" #my system, amgwo5
 page = requests.get(url)
 print (page.text)
 
-#what actually gets the rev count
-while True:
-	page = requests.get(url)
-	decoded = json.loads(page.text)
-	revs= decoded['EngineRevs']
-	print (revs)
-	time.sleep(.1)
-
 #GPIO
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(14, GPIO.OUT)
-GPIO.setup(15, GPIO.OUT)
-GPIO.setup(18, GPIO.OUT)
+GPIO.setup(2, GPIO.OUT)
+GPIO.setup(3, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
+GPIO.output(2,False)
+GPIO.output(3,False)
+GPIO.output(4,False)
 
 lean = 1780.0
 standard = 1820.0
 full = 1845.0
 
-#if the rev count is greater than lean, less than revs + 13.333, turn on 1
-if (revs >= lean) and (revs < 3000.0)
-	GPIO.output(14,True)
-	else GPIO.output(14,False)
-	
-if (revs >= lean) and (revs > (revs + 13.333))
-	GPIO.output(15,True)
-
-if (revs >= lean) and (revs > (revs + 26.666)) #satanwuzhere
-	GPIO.output(18,True)
-	
+while True:
+	#what actually gets the rev count
+	page = requests.get(url)
+	decoded = json.loads(page.text)
+	revs= decoded['EngineRevs']
+	print (revs)
+	#if the rev count is greater than lean, less than revs + 13.333, turn on 1
+	if (float(revs) >= float(lean)) and (float(revs) < 3000.0):
+		GPIO.output(2,True)
+	else:
+		GPIO.output(2,False)
+	if (float(revs) >= float(lean)) and (float(revs) > (float(revs) + 13.333)):
+		GPIO.output(3,True)
+	if (float(revs) >= float(lean)) and (float(revs) > (float(revs) + 26.666)) #satanwuzhere
+		GPIO.output(4,True)
+	time.sleep(.1)
